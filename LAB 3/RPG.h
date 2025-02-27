1aff7709ab3398df90344f2f61374e3f34be1110
DavidCalvillo831 <dcmadrid31@gmail.com>
2025-02-26 16:48:36 -0800

    INSERT CODE for rpg.h

--- LAB 3/RPG.h
+++ LAB 3/RPG.h
@@ -1,1 +1,40 @@
+#define RPG_H
 
+#include <string>
+
+#include namespace std; 
+
+const int SKILL_SIZE = 2; 
+
+class RPG {
+
+    public: 
+            RPG(); 
+            RPG(string name, int health, int strength, int defense, string type);
+
+            void setSkills(); 
+            void printAction(string action, RPG target); 
+            void updateHealth(int amount); 
+            void attack(RPG* opponent);
+            void useSkill(RPG* opponent); 
+
+            bool isAlive() const; 
+
+            string getName() const; 
+
+            int getHealth() const; 
+            int getStrength() const; 
+            int getDefense() const; 
+
+            
+            
+
+    private:
+            string name; 
+            int health; 
+            int strength; 
+            int defense; 
+
+            string type; 
+            string skills[SKILL_SIZE]; 
+};
